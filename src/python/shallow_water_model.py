f593ded73b86c9cd4e195b74a3b896d159024bd1
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2022-11-02 01:17:18 +0000

    Add python tests for shallow water model class
    Also add method to advance clock for state.
    Also add advancement of clock to model adv_nsteps.

--- src/python/shallow_water_model.py
+++ src/python/shallow_water_model.py
@@ -102,6 +102,8 @@ class ShallowWaterModel:
                     state.v[i,j] = _v_new[i,j]
                     state.h[i,j] = _h_new[i,j]
 
+            # Update the clock
+            state.advance_clock(self.dt)
 
     @gtscript.stencil(backend=backend)
     def _boundary_update(u     : FloatFieldIJ,
@@ -181,66 +183,66 @@ class ShallowWaterModel:
                     - 0.5 * dtdy * (h - b) * (v[0,1] - v[0,-1])
 
 
-comm = MPI.COMM_WORLD
-gc = ShallowWaterGeometryConfig.from_YAML_filename('shallow_water.yml')
-mc = ShallowWaterModelConfig.from_YAML_filename('shallow_water.yml')
-g = ShallowWaterGeometry(gc, comm)
-m = ShallowWaterModel(mc, g)
-
-# Create a state with a tsunami pulse in it to initialize field h
-h = np.empty((g.npx, g.npy), dtype=float)
-xmid = g.xmax / 2.0
-ymid = g.ymax / 2.0
-sigma = np.floor(g.xmax / 20.0)
-for i in range(g.xps, g.xpe + 1):
-    for j in range(g.yps, g.ype + 1):
-        dsqr = (i * g.dx - xmid)**2 + (j * g.dy - ymid)**2
-        h[i - g.xps,j - g.yps] = 5000.0 + np.exp(-dsqr / sigma**2) * (mc.h0 - 5000.0)
-s = ShallowWaterState(g, h=h)
-
-# Write the initial state
-s.write("state_0.nc")
-
-# Advance the model state
-#m.adv_nsteps(s, 200)
-
-# Write the final state
-#s.write("state_200.nc")
-
-def animate(n):
-    m.adv_nsteps(s, 1)
-    pc.set_array(s.h.data)
-
-import matplotlib.colors as colors
-class ColormapNormalize(colors.Normalize):
-    def __init__(self, vmin=None, vmax=None, v1=None, v2=None, clip=False):
-        self.v1 = v1
-        self.v2 = v2
-        super().__init__(vmin, vmax, clip)
-
-    def __call__(self, value, clip=None):
-        # I'm ignoring masked values and all kinds of edge cases to make a
-        # simple example...
-        # Note also that we must extrapolate beyond vmin/vmax
-        x, y = [self.vmin, self.v1, self.v2, self.vmax], [0, 0.1, 0.9, 1.]
-        return np.ma.masked_array(np.interp(value, x, y,
-                                            left=-np.inf, right=np.inf))
-
-    def inverse(self, value):
-        y, x = [self.vmin, self.v1, self.v2, self.vmax], [0, 0.1, 0.9, 1]
-        return np.interp(value, x, y, left=-np.inf, right=np.inf)
-
-
-
-# Plot animation
-X, Y = np.meshgrid(np.linspace(0, g.xmax, g.npx), np.linspace(0, g.ymax, g.npy))
-fig, axs = plt.subplots()
-norm = ColormapNormalize(vmin=4990, vmax=5030, v1=4995, v2=5005)
-pc = axs.pcolormesh(X, Y, s.h.data, cmap='nipy_spectral', norm=norm)
-cb = fig.colorbar(pc, ax=axs, extend='both')
-fig.savefig('test0.png')
-anim = animation.FuncAnimation(fig, animate, interval=125, frames=500)
-anim.save('test.gif')
-fig.savefig('test500.png')
-s.write("state_500.nc")
+#comm = MPI.COMM_WORLD
+#gc = ShallowWaterGeometryConfig.from_YAML_filename('shallow_water.yml')
+#mc = ShallowWaterModelConfig.from_YAML_filename('shallow_water.yml')
+#g = ShallowWaterGeometry(gc, comm)
+#m = ShallowWaterModel(mc, g)
+#
+## Create a state with a tsunami pulse in it to initialize field h
+#h = np.empty((g.npx, g.npy), dtype=float)
+#xmid = g.xmax / 2.0
+#ymid = g.ymax / 2.0
+#sigma = np.floor(g.xmax / 20.0)
+#for i in range(g.xps, g.xpe + 1):
+#    for j in range(g.yps, g.ype + 1):
+#        dsqr = (i * g.dx - xmid)**2 + (j * g.dy - ymid)**2
+#        h[i - g.xps,j - g.yps] = 5000.0 + np.exp(-dsqr / sigma**2) * (mc.h0 - 5000.0)
+#s = ShallowWaterState(g, h=h)
+#
+## Write the initial state
+#s.write("state_0.nc")
+#
+## Advance the model state
+##m.adv_nsteps(s, 200)
+#
+## Write the final state
+##s.write("state_200.nc")
+#
+#def animate(n):
+#    m.adv_nsteps(s, 1)
+#    pc.set_array(s.h.data)
+#
+#import matplotlib.colors as colors
+#class ColormapNormalize(colors.Normalize):
+#    def __init__(self, vmin=None, vmax=None, v1=None, v2=None, clip=False):
+#        self.v1 = v1
+#        self.v2 = v2
+#        super().__init__(vmin, vmax, clip)
+#
+#    def __call__(self, value, clip=None):
+#        # I'm ignoring masked values and all kinds of edge cases to make a
+#        # simple example...
+#        # Note also that we must extrapolate beyond vmin/vmax
+#        x, y = [self.vmin, self.v1, self.v2, self.vmax], [0, 0.1, 0.9, 1.]
+#        return np.ma.masked_array(np.interp(value, x, y,
+#                                            left=-np.inf, right=np.inf))
+#
+#    def inverse(self, value):
+#        y, x = [self.vmin, self.v1, self.v2, self.vmax], [0, 0.1, 0.9, 1]
+#        return np.interp(value, x, y, left=-np.inf, right=np.inf)
+#
+#
+#
+## Plot animation
+#X, Y = np.meshgrid(np.linspace(0, g.xmax, g.npx), np.linspace(0, g.ymax, g.npy))
+#fig, axs = plt.subplots()
+#norm = ColormapNormalize(vmin=4990, vmax=5030, v1=4995, v2=5005)
+#pc = axs.pcolormesh(X, Y, s.h.data, cmap='nipy_spectral', norm=norm)
+#cb = fig.colorbar(pc, ax=axs, extend='both')
+#fig.savefig('test0.png')
+#anim = animation.FuncAnimation(fig, animate, interval=125, frames=500)
+#anim.save('test.gif')
+#fig.savefig('test500.png')
+#s.write("state_500.nc")
 
